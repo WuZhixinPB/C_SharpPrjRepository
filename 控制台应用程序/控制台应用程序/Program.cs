9d36163067f7bd73fb51f712b776b31a9439afc9
WuZhixinPB <847708299@qq.com>
2018-11-25 20:41:18 +0800

    第一个窗口程序

--- 控制台应用程序/控制台应用程序/Program.cs
+++ 控制台应用程序/控制台应用程序/Program.cs
@@ -13,26 +13,31 @@ namespace 控制台应用程序
             Console.WriteLine("hello world!!");
             Console.ReadLine();
             Console.WriteLine("请输入以下计算的结果,按回车结束");
-            
 
             Random randomClass = new Random();
-            int number1, number2,result;
-            number1 = randomClass.Next(1, 100);
-            number2 = randomClass.Next(1, 100);
-            result = number1 + number2;
+            int number1, number2, result;
+            while (true)
+            {
+                number1 = randomClass.Next(1, 100);
+                number2 = randomClass.Next(1, 100);
+                result = number1 + number2;
 
-            Console.Write("{0}+{1}=", number1, number2);
-            int userResult;
-            userResult = Convert.ToInt16(Console.ReadLine());
-            if (userResult == result)
-            {
-                Console.WriteLine("right!");
-            }
-            else
-            {
-                Console.WriteLine("erron!");
+                Console.Write("{0}+{1}=", number1, number2);
+                int userResult;
+                userResult = Convert.ToInt16(Console.ReadLine());
+                if (userResult == result)
+                {
+                    Console.WriteLine("right!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("erron!");
+                }
+                
             }
             Console.ReadLine();
+
         }
     }
 }
