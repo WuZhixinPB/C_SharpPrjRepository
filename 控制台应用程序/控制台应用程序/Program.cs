6dd3891dfa6839901dfcff0b9bf034a818f81a51
WuZhixinPB <847708299@qq.com>
2018-11-25 13:01:09 +0800

    一个随机计算的验证码

--- 控制台应用程序/控制台应用程序/Program.cs
+++ 控制台应用程序/控制台应用程序/Program.cs
@@ -13,7 +13,26 @@ namespace 控制台应用程序
             Console.WriteLine("hello world!!");
             Console.ReadLine();
             Console.WriteLine("请输入以下计算的结果,按回车结束");
-            Console.ReadKey();
+            
+
+            Random randomClass = new Random();
+            int number1, number2,result;
+            number1 = randomClass.Next(1, 100);
+            number2 = randomClass.Next(1, 100);
+            result = number1 + number2;
+
+            Console.Write("{0}+{1}=", number1, number2);
+            int userResult;
+            userResult = Convert.ToInt16(Console.ReadLine());
+            if (userResult == result)
+            {
+                Console.WriteLine("right!");
+            }
+            else
+            {
+                Console.WriteLine("erron!");
+            }
+            Console.ReadLine();
         }
     }
 }
